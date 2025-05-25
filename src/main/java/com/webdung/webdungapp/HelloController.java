f4217b3830c552b8bf70509a65b4e83c42d4182a
pfauju <04juli04@gmail.com>
2025-05-25 17:41:37 +0200

    Test deployment hook

--- src/main/java/com/webdung/webdungapp/HelloController.java
+++ src/main/java/com/webdung/webdungapp/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Willkommen bei Webdung-dung-dung Sahurrrrrrr!";
+        return "Willkommen bei Webdung-dung-dung Sahurrrrrrrr!";
     }
 }
