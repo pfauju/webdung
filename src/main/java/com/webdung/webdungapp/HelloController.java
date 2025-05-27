9d11b9e773995564b27b295f3bd6056fd266c594
pfauju <04juli04@gmail.com>
2025-05-27 16:01:10 +0200

    Beschreibung

--- src/main/java/com/webdung/webdungapp/HelloController.java
+++ src/main/java/com/webdung/webdungapp/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Willkommen bei Webdung!";
+    public String index() {
+        return "index";
     }
 }
