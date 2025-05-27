742729a38b4d46de098b63a8079f9316ca900743
pfauju <04juli04@gmail.com>
2025-05-27 16:04:42 +0200

    Beschreibung

--- src/main/java/com/webdung/webdungapp/HelloController.java
+++ src/main/java/com/webdung/webdungapp/HelloController.java
@@ -1,9 +1,10 @@
 package com.webdung.webdungapp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class HelloController {
 
     @GetMapping("/")
