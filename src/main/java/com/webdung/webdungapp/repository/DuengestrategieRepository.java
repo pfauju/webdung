ca0993d76bc76e04e400bf8bbd8a764eba3c7118
Websi5 <christophweber704@gmail.com>
2025-07-06 17:36:50 +0200

    Änderung von Freund X

--- src/main/java/com/webdung/webdungapp/repository/DuengestrategieRepository.java
+++ src/main/java/com/webdung/webdungapp/repository/DuengestrategieRepository.java
@@ -1,23 +1,8 @@
 package com.webdung.webdungapp.repository;
 
-import com.webdung.webdungapp.model.Duengestrategie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.webdung.webdungapp.model.Duengestrategie;
 
-@Repository
 public interface DuengestrategieRepository extends JpaRepository<Duengestrategie, Long> {
-    
-    // Strategien nach Kultur finden
-    List<Duengestrategie> findByKultur(String kultur);
-    
-    // Strategien nach Dünger finden
-    List<Duengestrategie> findByDuenger(String duenger);
-    
-    // Strategien nach Name finden
-    List<Duengestrategie> findByStrategienameContainingIgnoreCase(String name);
-    
-    // Alle Strategien sortiert nach Erstellungsdatum
-    List<Duengestrategie> findAllByOrderByErstelltAmDesc();
 }
